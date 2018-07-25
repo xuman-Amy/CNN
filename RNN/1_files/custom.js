97df4b997677cdcb42df2a2acad1b38ddd1fa5ed
xuman-Amy <1445357489@qq.com>
2018-07-25 11:46:47 +0800

    all

---
+++ RNN/1_files/custom.js
@@ -0,0 +1,96 @@
+require(["codemirror/keymap/sublime", "notebook/js/cell", "base/js/namespace"],
+    function(sublime_keymap, cell, IPython) {
+        // setTimeout(function(){ // uncomment line to fake race-condition
+        cell.Cell.options_default.cm_config.keyMap = 'sublime';
+        var cells = IPython.notebook.get_cells();
+        for(var cl=0; cl< cells.length ; cl++){
+            cells[cl].code_mirror.setOption('keyMap', 'sublime');
+        }
+ 
+        // }, 1000)// uncomment  line to fake race condition 
+    } 
+);
+
+
+
+/**
+*
+*
+* Placeholder for custom user javascript
+* mainly to be overridden in profile/static/custom/custom.js
+* This will always be an empty file in IPython
+*
+* User could add any javascript in the `profile/static/custom/custom.js` file.
+* It will be executed by the ipython notebook at load time.
+*
+* Same thing with `profile/static/custom/custom.css` to inject custom css into the notebook.
+*
+*
+* The object available at load time depend on the version of IPython in use.
+* there is no guaranties of API stability.
+*
+* The example below explain the principle, and might not be valid.
+*
+* Instances are created after the loading of this file and might need to be accessed using vents:
+*     define([
+*        'base/js/namespace',
+*        'base/js/events'
+*     ], function(IPython, events) {
+*         events.on("app_initialized.NotebookApp", function () {
+*             IPython.keyboard_manager....
+*         });
+*     });
+*
+* __Example 1:__
+*
+* Create a custom button in toolbar that execute `%qtconsole` in kernel
+* and hence open a qtconsole attached to the same kernel as the current notebook
+*
+*    define([
+*        'base/js/namespace',
+*        'base/js/events'
+*    ], function(IPython, events) {
+*        events.on('app_initialized.NotebookApp', function(){
+*            IPython.toolbar.add_buttons_group([
+*                {
+*                    'label'   : 'run qtconsole',
+*                    'icon'    : 'fa-terminal', // select your icon from ttp://fortawesome.github.io/Font-Awesome/icons
+*                    'callback': function () {
+*                        IPython.notebook.kernel.execute('%qtconsole')
+*                    }
+*                }
+*                // add more button here if needed.
+*                ]);
+*        });
+*    });
+*
+* __Example 2:__
+*
+* At the completion of the dashboard loading, load an unofficial javascript extension
+* that is installed in profile/static/custom/
+*
+*    define([
+*        'base/js/events'
+*    ], function(events) {
+*        events.on('app_initialized.DashboardApp', function(){
+*            require(['custom/unofficial_extension.js'])
+*        });
+*    });
+*
+* __Example 3:__
+*
+*  Use `jQuery.getScript(url [, success(script, textStatus, jqXHR)] );`
+*  to load custom script into the notebook.
+*
+*    // to load the metadata ui extension example.
+*    $.getScript('/static/notebook/js/celltoolbarpresets/example.js');
+*    // or
+*    // to load the metadata ui extension to control slideshow mode / reveal js for nbconvert
+*    $.getScript('/static/notebook/js/celltoolbarpresets/slideshow.js');
+*
+*
+* @module IPython
+* @namespace IPython
+* @class customjs
+* @static
+*/
